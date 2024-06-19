62df17b0f2b4ea04fc1db83f6d4e73ae9f164bb9
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2024-06-19 23:03:06 +0300

    feat: cors headers

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-import express from "express";
-import fs from "fs";
-
+import express from 'express';
+import fs from 'fs';
+import cors from 'cors';
 const app = express();
 
-const users = JSON.parse(fs.readFileSync("users.json", "utf-8"));
-const albums = JSON.parse(fs.readFileSync("albums.json", "utf-8"));
-const photos = JSON.parse(fs.readFileSync("photos.json", "utf-8"));
+const users = JSON.parse(fs.readFileSync('users.json', 'utf-8'));
+const albums = JSON.parse(fs.readFileSync('albums.json', 'utf-8'));
+const photos = JSON.parse(fs.readFileSync('photos.json', 'utf-8'));
 
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['X-Requested-With', 'content-type'],
+    credentials: true
+  })
+);
 // Эндпоинты
-app.get("/users/:userId", (req, res) => {
-  const user = users.find((user) => user.id === req.params.userId);
+app.get('/users/:userId', (req, res) => {
+  const user = users.find(user => user.id === req.params.userId);
   if (user) {
     res.json(user);
   } else {
-    res.status(404).send("Пользователь не найден");
+    res.status(404).send('Пользователь не найден');
   }
 });
 
-app.get("/users", (req, res) => {
+app.get('/users', (req, res) => {
   res.json(users);
 });
 
-app.get("/albums/:userId", (req, res) => {
-  const userAlbums = albums.filter(
-    (album) => album.userId === req.params.userId
-  );
+app.get('/albums/:userId', (req, res) => {
+  const userAlbums = albums.filter(album => album.userId === req.params.userId);
   res.json(userAlbums);
 });
 
-app.get("/photos/:albumId", (req, res) => {
+app.get('/photos/:albumId', (req, res) => {
   const albumPhotos = photos.filter(
-    (photo) => photo.albumId === req.params.albumId
+    photo => photo.albumId === req.params.albumId
   );
   res.json(albumPhotos);
 });
